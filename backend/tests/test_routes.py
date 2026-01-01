62ee087819ec16fa3d364a789a7b793bf3dbab23
lily-it <122046583+lily-it@users.noreply.github.com>
2026-01-01 22:19:12 +0530

    Update test_routes.py

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -6,8 +6,8 @@ client = TestClient(app)
 
 def test_get_recommendations():
     response = client.get("/recommendations/123")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+    assert response.status_code == 200 # assert function checks if the condition is true
+    assert isinstance(response.json(), list) #.jason() data jason file ko python object mein convert karta hai 
 
 def test_get_trending():
     response = client.get("/trending")
@@ -18,3 +18,4 @@ def test_get_similar_items():
     response = client.get("/similar-items/987")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+
